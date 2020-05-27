3e06656d78190ef7a8007a28bf886fc8c1f10447
황성영 <ghkdtjddud@gmail.com>
2020-05-27 11:39:15 +0900

    init

---
+++ src/main/kotlin/com/larva/emul/config/Swagger2Config.kt
@@ -0,0 +1,37 @@
+package com.larva.emul.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class Swagger2Config {
+
+    @Bean
+    fun createRestApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("larva emul")
+                .description("larva emul")
+                .termsOfServiceUrl("https://www.larva.com")
+                .contact(Contact("larva", "https://larva.com", "ghkdtjddud@gmail.com"))
+                .version("1.0.0")
+                .build()
+    }
+}
